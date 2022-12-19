711dd3ea96e9ce7c688f1941340dfc758b1439bc
DzianisShapel <dzianis283@gmail.com>
2022-12-19 16:54:52 +0300

    update pom.xml

--- src/main/java/org/askomdch/factory/DriverManagerFactory.java
+++ src/main/java/org/askomdch/factory/DriverManagerFactory.java
@@ -7,14 +7,16 @@ public class DriverManagerFactory {
     public static DriverManager getManager(DriverType driverType) {
         //based on driverType we are sending it is returning a new object of the corresponding class
         // which is implementing DriverManager interface
+        DriverManager manager;
         switch (driverType) {
-            case CHROME -> {
-                return new ChromeDriverManager();
-            }
-            case FIREFOX -> {
-                return new FirefoxDriverManager();
-            }
-            default -> throw new IllegalStateException("Invalid driver: " + driverType);
+            case CHROME: manager = new ChromeDriverManager();
+            break;
+            case FIREFOX:
+                manager =  new FirefoxDriverManager();
+            break;
+            default:
+                throw new IllegalStateException("Invalid driver: " + driverType);
         }
+        return manager;
     }
 }
